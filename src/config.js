2e0a93ac75aa6d101c345b24ad32e9e99ade6689
play.js <git@gituser.com>
2021-07-05 10:05:30 -0500

    implement fire base

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+  apiKey: "AIzaSyBDrik-NI7CAy0_1ZpLwUXyKf1op3DwKkU",
+  authDomain: "fire-base-demo-d699d",
+  databaseURL: "https://fire-base-demo-d699d-default-rtdb.firebaseio.com/"
+};
+
+export default config;
